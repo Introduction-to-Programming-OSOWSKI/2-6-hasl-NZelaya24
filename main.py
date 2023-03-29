1ce83d68ea07b804dec926db8904de1e849ecc2a
NZelaya24 <825123@mystma.org>
2023-03-29 14:54:14 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL (w):
+    for i in range (0, len (w), 1):
+        if w[i] == "l":
+            return True
+    else :
+      return False
+
+print (hasL ("dog"))
+print (hasL ("alabama"))
